65e46290c96ba3ea7e9551d663f80ca7ed89670d
Joe Julian <me@joejulian.name>
2016-10-26 15:18:49 -0700

    Bump release version to 0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "splitmount",
-    version = "0.1.1",
+    version = "0.1.2",
     author = "Joe Julian",
     author_email = "me@joejulian.name",
     description = ( "Admin tool to aid in the recovery of split-brain file entries in GlusterFS" ),
